d7fa3e147d283ffb117d61038175c25c7a614adf
arrowrowe <arrowrowe@gmail.com>
2016-05-21 21:27:01 +0800

    :cactus: solve 22

---
+++ src/22.js
@@ -0,0 +1,12 @@
+const io = require('../util/io');
+const {log, range, plus} = require('../util/fn');
+
+const value = s => Array.from(s).reduce((a, b) => a + b.charCodeAt(0) - 64, 0)
+
+io.get('https://projecteuler.net/project/resources/p022_names.txt')
+  .get('body')
+  .then(raw => raw.slice(1, -1).split('","'))
+  .call('sort', (a, b) => a > b ? 1 : -1)
+  .map((name, index) => value(name) * (index + 1))
+  .reduce(plus, 0)
+  .then(log);
